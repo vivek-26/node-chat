5557e94ad93c7a141e63036e2b9ee6518aab74c9
Vivek Kumar <vivek.kumar26@live.com>
2017-12-30 13:29:59 +0530

    added socket.io

---
+++ public/main.js
@@ -0,0 +1,2 @@
+// Initiate a request (Socket.io) - Open a new websocket and keep that open.
+var socket = io();
--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 // Dependencies
 const express = require('express');
 const debug = require('debug')('node-chat');
+const socketIO = require('socket.io');
+const http = require('http');
 const path = require('path');
 
 // Public path - static files
@@ -14,11 +16,22 @@ debug(`PORT - ${PORT}`)
 // Create express application
 const app = express();
 
+// Create HTTP Server
+const server = http.createServer(app);
+
+// Create WebSocket Server
+const io = socketIO(server);
+
+// Listen for a new connection, 'socket' param represents individual socket.
+io.on('connection', (socket) => {
+    debug('New user connected');
+});
+
 // Serve static files
 app.use(express.static(publicPath));
 
 // Listen on configured port
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Node Chat Application started on PORT - ${PORT}`);
     debug(`Node Chat Application started on PORT - ${PORT}`);
 });
