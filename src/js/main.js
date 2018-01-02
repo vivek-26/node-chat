88715bec1da8f65e19142f072c90b41c021f7fc6
Vivek Kumar <vivek.kumar26@live.com>
2018-01-02 21:03:00 +0530

    add form for messages & show incoming messages in browser

--- src/js/main.js
+++ src/js/main.js
@@ -18,4 +18,22 @@ socket.on('disconnect', () => {
 // Listen for new message event
 socket.on('newMessage', (message) => {
     console.log('New Message Received - ', message);
+    const ol = document.querySelector('#messages');
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(`${message.from}: ${message.text}`));
+    ol.appendChild(li);
+});
+
+// Handle form submit
+const formSubmit$ = Rx.Observable.fromEvent(document.querySelector('#message-form'), 'submit');
+formSubmit$.subscribe({
+    next: (event) => {
+        event.preventDefault();
+        socket.emit('createMessage', {
+            'from': 'User',
+            'text': document.querySelector('#message').value
+        }, () => {});
+    },
+    error: (err) => console.log('Oops an error occurred. Details - ', err),
+    complete: () => console.log('Completed!')
 });
