e777e85b75ccb5eec8b42fbe481cc49c7bd5e7d7
Vivek Kumar <vivek.kumar26@live.com>
2018-01-08 19:59:01 +0530

    setup join page & webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 const extractPlugin = new ExtractTextPlugin({
-    filename: '[name].css'
+    filename: 'main.css'
 });
 
 const path = require('path');
